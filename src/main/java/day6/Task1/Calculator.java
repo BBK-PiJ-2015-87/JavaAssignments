f7c10eebcb9408d4e93db0292b26c9375865eff0
BBK-PiJ-2015-87 <londonbim@gmail.com>
2015-10-22 22:17:49 +0100

    Initial commit

---
+++ src/main/java/day6/Task1/Calculator.java
@@ -0,0 +1,26 @@
+package day6.Task1;
+
+/**
+ * Created by Workstation on 22/10/2015.
+ */
+public class Calculator {
+    public static void add(int a, int b) {
+        System.out.println(a+b);
+    }
+
+    public static void subtract(int a, int b) {
+        System.out.println(a-b);
+    }
+
+    public static void multiply(int a, int b) {
+        System.out.println(a*b);
+    }
+
+    public static void divide(int a, int b) {
+        System.out.println(a/b);
+    }
+
+    public static void modulo(int a, int b) {
+        System.out.println(a%b);
+    }
+}
---
+++ src/main/java/day6/Task1/Task1.java
@@ -0,0 +1,14 @@
+package day6.Task1;
+
+/**
+ * Created by Workstation on 22/10/2015.
+ */
+public class Task1 {
+    public static void main(String[] args) {
+        Calculator.add(2, 3);
+        Calculator.divide(2, 3);
+        Calculator.modulo(5, 3);
+        Calculator.multiply(2, 3);
+        Calculator.subtract(2, 3);
+    }
+}
