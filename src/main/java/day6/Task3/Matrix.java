33afe56620d1cb7fa8ef2065e91da7af8e606f77
BBK-PiJ-2015-87 <londonbim@gmail.com>
2015-10-23 23:05:24 +0100

    Updated matrix class

--- src/main/java/day6/Task3/Matrix.java
+++ src/main/java/day6/Task3/Matrix.java
@@ -1,12 +1,16 @@
 package day6.Task3;
 
 /**
- * Created by Workstation on 23/10/2015.
+ * Created by Vladimirs Ivanovs on 23/10/2015.
  */
 public class Matrix {
+    private int rows;
+    private int columns;
     private int[][] matrix;
 
     public Matrix(int x, int y) {
+        rows = x;
+        columns = y;
         matrix = new int[x][y];
         initialize();
     }
@@ -28,29 +32,68 @@ public class Matrix {
     public void print() {
         for (int[] sub  : matrix) {
             for (int a : sub) {
-                System.out.print(a + " ");
+                System.out.print(a + "\t");
             }
             System.out.println();
         }
     }
 
+    /**
+     * Sets value of an element
+     *
+     * @param row number
+     * @param column number
+     * @param value to be set
+     */
     public void setElement(int row, int column, int value) {
         matrix[row][column] = value;
     }
 
     /**
-     * Set values for a given row
+     * Sets values for a given row
      * @param row number to be updated
-     * @param value sequence of numbers (e.g. 1,2,3,5)
+     * @param value comma separated sequence of numbers (e.g. 1,2,3,5)
      */
     public void setRow(int row, String value) {
         String[] array = value.split(",");
-        if (array.length == matrix[row].length) {
-            for (int i = 0; i < array.length; i++) {
+        if (array.length == columns) {
+            for (int i = 0; i < columns; i++) {
                 matrix[row][i] = Integer.parseInt(array[i]);
             }
         } else {
             System.out.println("Error. Please re-enter numbers.");
         }
+    }
+
+    /**
+     * Sets values for a given column
+     * @param column number to be updated
+     * @param value comma separated sequence of numbers
+     */
+    public void setColumn(int column, String value) {
+        String[] array = value.split(",");
+        if (array.length == rows) {
+            for (int i = 0; i < rows; i++) {
+                matrix[i][column] = Integer.parseInt(array[i]);
+            }
+        } else {
+            System.out.println("Error. Please re-enter numbers.");
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuffer string = new StringBuffer();
+        string.append("[");
+        for (int[] row : matrix) {
+            string.append("[");
+            for (int pos =0; pos < row.length; pos++) {
+                string.append(row[pos]);
+                if (pos < row.length -1 ) string.append(",");
+            }
+            string.append("]");
+        }
+        string.append("]");
+        return string.toString();
     }
 }
--- src/main/java/day6/Task3/Task3.java
+++ src/main/java/day6/Task3/Task3.java
@@ -5,9 +5,11 @@ package day6.Task3;
  */
 public class Task3 {
     public static void main(String[] args) {
-        Matrix matrix = new Matrix(10, 4);
+        Matrix matrix = new Matrix(8, 4);
         matrix.setElement(1,2,200);
         matrix.setRow(3, "5,5,5,5");
+        matrix.setColumn(1, "3,3,3,3,3,3,3,3");
         matrix.print();
+        System.out.println(matrix.toString());
     }
 }
