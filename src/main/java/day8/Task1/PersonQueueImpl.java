cf4057bcb9054278b8796262d0fe4f779111f3d9
BBK-PiJ-2015-87 <londonbim@gmail.com>
2015-11-13 12:09:35 +0000

    Implementation of PersonQueue interface

---
+++ src/main/java/day8/Task1/PersonQueueImpl.java
@@ -0,0 +1,14 @@
+package day8.Task1;
+
+/**
+ * Created by Workstation on 13/11/2015.
+ */
+public class PersonQueueImpl implements PersonQueue {
+    public void insert(Person person) {
+
+    }
+
+    public Person retrieve() {
+        return null;
+    }
+}
