ccd9017febd452231264d0cde29a7bea5b3c5fdf
BBK-PiJ-2015-87 <londonbim@gmail.com>
2015-11-14 21:57:49 +0000

    Updated insert method

--- src/main/java/day8/Task1/PersonQueueImpl.java
+++ src/main/java/day8/Task1/PersonQueueImpl.java
@@ -1,14 +1,35 @@
 package day8.Task1;
 
 /**
+ * Queue as a linked list.
+ *
  * Created by Workstation on 13/11/2015.
  */
 public class PersonQueueImpl implements PersonQueue {
-    public void insert(Person person) {
 
+    private int size = 0;
+    private Person first; //first element of the queue.
+    private Person last; //last element of the queue.
+
+    public void insert(Person person) {
+        if (isEmpty()) {
+            last = person;
+        } else {
+            person.setNext(first);
+        }
+        first = person;
     }
 
     public Person retrieve() {
-        return null;
+
+        return last;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    private boolean isEmpty() {
+        return size == 0;
     }
 }
