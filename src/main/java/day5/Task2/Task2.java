0b7197e6164e8398749003bc3edd7171f71e99ff
Vladimirs Ivanovs <londonbim@gmail.com>
2015-11-24 16:53:05 +0000

    Implemented factorial recursively

--- src/main/java/day5/Task2/Task2.java
+++ src/main/java/day5/Task2/Task2.java
@@ -1,7 +1,19 @@
 package day5.Task2;
 
-/**
- * Created by vladimirsivanovs on 24/11/2015.
- */
 public class Task2 {
+
+    public int factorialRec(int n) {
+        if (n == 1) {
+            return 1;
+        } else {
+            int result = n * factorialRec(n - 1);
+            return result;
+        }
+    }
+
+    public static void main(String[] args) {
+        Task2 app = new Task2();
+        System.out.println(app.factorialRec(10));
+    }
 }
+
