a001de63093f7eb4ab89493fc40396e0bed04c2f
Vladimirs Ivanovs <londonbim@gmail.com>
2015-11-24 10:40:53 +0000

    Updated exception order

--- src/main/java/day15/Task2/Task2.java
+++ src/main/java/day15/Task2/Task2.java
@@ -1,7 +1,24 @@
 package day15.Task2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by vladimirsivanovs on 24/11/2015.
  */
 public class Task2 {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList();
+        int newElement = 10;
+        try {
+            list.add(newElement);
+        } catch (NullPointerException ex) {
+            ex.printStackTrace();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 }
+
+
+
