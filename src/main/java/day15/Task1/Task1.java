8dc3dbf5e831a221a070eb5cdebb1bd1d27fbef0
Vladimirs Ivanovs <londonbim@gmail.com>
2015-11-24 10:32:02 +0000

    Initial commit

---
+++ src/main/java/day15/Task1/Task1.java
@@ -0,0 +1,43 @@
+package day15.Task1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by vladimirsivanovs on 24/11/2015.
+ */
+public class Task1 {
+    public void launch(int userInput) {
+        List<Integer> intList = new ArrayList<Integer>();
+        intList.add(1);
+        intList.add(2);
+        intList.add(3);
+        intList.add(4);
+        intList.add(5);
+        intList.add(6);
+        try {
+            intList.remove(0);
+            System.out.println(intList.get(userInput));
+            intList.remove(0);
+            System.out.println(intList.get(userInput));
+            intList.remove(0);
+            System.out.println(intList.get(userInput));
+            intList.remove(0);
+            System.out.println(intList.get(userInput));
+            intList.remove(0);
+            System.out.println(intList.get(userInput));
+            intList.remove(0);
+            System.out.println(intList.get(userInput));
+            intList.remove(0);
+            System.out.println(intList.get(userInput));
+        } catch (IndexOutOfBoundsException ex) {
+            System.out.println("Exception");
+        }
+    }
+
+    public static void main(String[] args) {
+        Task1 task = new Task1();
+        int input = 1;
+        task.launch(input);
+    }
+}
---
+++ src/main/java/day15/Task2/Task2.java
@@ -0,0 +1,7 @@
+package day15.Task2;
+
+/**
+ * Created by vladimirsivanovs on 24/11/2015.
+ */
+public class Task2 {
+}
