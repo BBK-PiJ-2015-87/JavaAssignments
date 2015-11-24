dfe92c3d4ac85e9688c2b0fa107f9778f40252d2
Vladimirs Ivanovs <londonbim@gmail.com>
2015-11-24 16:32:43 +0000

    Updated class usage

--- src/main/java/day11/Task1/Task1.java
+++ src/main/java/day11/Task1/Task1.java
@@ -1,7 +1,11 @@
 package day11.Task1;
 
-/**
- * Created by vladimirsivanovs on 24/11/2015.
- */
 public class Task1 {
+    public static void main(String[] args) {
+        Comparator comparator = new Comparator();
+
+        System.out.println(comparator.getMax(1, 2));
+        System.out.println(comparator.getMax(10.2, 10.3));
+        System.out.println(comparator.getMax("10", "111"));
+    }
 }
--- src/main/java/day11/Task2/Task2.java
+++ src/main/java/day11/Task2/Task2.java
@@ -1,14 +1,7 @@
 package day11.Task2;
 
-import day11.Task1.Comparator;
-
-
 public class Task2 {
     public static void main(String[] args) {
-        Comparator comparator = new Comparator();
 
-        System.out.println(comparator.getMax(1, 2));
-        System.out.println(comparator.getMax(10.2, 10.3));
-        System.out.println(comparator.getMax("10", "111"));
     }
 }
