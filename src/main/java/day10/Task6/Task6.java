1844409e20e78d982f72b885d81f88e4ee2541e5
Vladimirs Ivanovs <londonbim@gmail.com>
2015-11-24 15:08:25 +0000

    Tested methods of lecturer class

--- src/main/java/day10/Task6/Task6.java
+++ src/main/java/day10/Task6/Task6.java
@@ -1,4 +1,10 @@
 package day10.Task6;
 
 public class Task6 {
+    public static void main(String[] args) {
+        Lecturer lecturer = new Lecturer("Mathematician");
+
+        System.out.println(lecturer.getName());
+        lecturer.doResearch("medicine");
+    }
 }
